5a3895a2f18402e695c3845305ec0e240afd7c52
Pallishree Senapati <Pallishree.Senapati@tcgdigital.com>
2026-09-14 21:35:41 +0530

    Expand Playwright fundamentals test suite

--- tests/01_Basics/219_tta-check.spec.ts
+++ tests/01_Basics/219_tta-check.spec.ts
@@ -3,10 +3,10 @@ import { test, expect } from '@playwright/test';
 test('test', async ({ page }) => {
   await page.goto('https://app.thetestingacademy.com/playwright/multiple_element_filter');
   await page.getByRole('textbox', { name: 'Email Address' }).click();
-  await page.getByRole('textbox', { name: 'Email Address' }).fill('pallishree@gmail.com');
+  await page.getByRole('textbox', { name: 'Email Address' }).fill('pramod');
   await page.getByRole('textbox', { name: 'Password' }).click();
-  await page.getByRole('textbox', { name: 'Password' }).fill('Pass123');
-  await page.getByRole('checkbox', { name: 'Remember me' }).check();
+  await page.getByRole('textbox', { name: 'Password' }).fill('123');
   await page.getByTestId('login-button').click();
-  await page.waitForTimeout(5000);
+//   await page.waitForTimeout(50000);
 });
+
